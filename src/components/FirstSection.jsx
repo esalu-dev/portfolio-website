78c300a95ddbc8681e98ed51b8cc3ce2f5a73ffb
esalu-dev <esalu88@outlook.com>
2023-08-17 12:20:51 -0600

    Added Music Composer to Title

--- src/components/FirstSection.jsx
+++ src/components/FirstSection.jsx
@@ -3,7 +3,7 @@ import { LinkedInButton } from "./LinkedInButton"
 
 export function FirstSection(){
    return(
-      <section className="w-full h-[600px] flex flex-col items-center pt-10">
+      <section className="w-full h-[600px] flex flex-col items-center pt-20 mb-10">
          <div className="w-64 relative">
             <img className="z-10 rounded-full relative border-4 border-emerald-600 dark:border-light-green" src="https://avatars.githubusercontent.com/u/66653779?v=4" alt="Fotografía de Emilio Salas"/>
             <span className=" w-64 h-64 rounded-full bg-gradient-to-bl from-aqua-green-700 to-pastel-green-800 dark:bg-gradient-to-bl dark:from-pastel-green-800 dark:to-emerald absolute top-4 left-4 opacity-70 shadow-2xl"></span>
@@ -11,7 +11,7 @@ export function FirstSection(){
          <aside className="text-center mt-14 mb-6">
             <p className="dark:text-light-green-800 font-bold text-lg text-emerald-800">Hello! I&apos;m</p>
             <h2 className="font-bold text-4xl dark:text-light-green-200">Emilio Salas</h2>
-            <h3 className="dark:text-pastel-green font-bold text-pastel-green-900 text-2xl">Front-End Developer</h3>
+            <h3 className="dark:text-pastel-green font-bold text-pastel-green-900 text-2xl">Front-End Developer & <span className="dark:text-white text-aqua-green-800">Music Composer</span></h3>
          </aside>
          <footer className="flex gap-2">
             <GithubButton/>
